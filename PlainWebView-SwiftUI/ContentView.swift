4bbb9af67ff74fda341edf82448cab889cac155c
S.Oshio <skw398@gmail.com>
2023-10-24 16:44:17 +0900

    done

--- PlainWebView-SwiftUI/ContentView.swift
+++ PlainWebView-SwiftUI/ContentView.swift
@@ -6,16 +6,111 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
+    @State var searchBarText: String = ""
+    
+    @State private var action: WebView.Action = .none
+    @State private var canGoBack: Bool = false
+    @State private var canGoForward: Bool = false
+    @State private var currentURL: URL? = nil
+    @State private var estimatedProgress: Double = 0.0
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+
+        var cancellables = Set<AnyCancellable>()
+        
+        let webView = WebView(
+            action: $action,
+            canGoBack: $canGoBack,
+            canGoForward: $canGoForward,
+            currentURL: $currentURL,
+            setWebViewHandler: { webView in
+                webView.publisher(for: \.estimatedProgress)
+                    .receive(on: DispatchQueue.main)
+                    .sink { value in
+                        estimatedProgress = value
+                    }
+                    .store(in: &cancellables)
+            }
+        )
+
+        NavigationView {
+            VStack(spacing: 0) {
+
+                webView
+                    .onChange(of: currentURL) { newValue in
+                        if let urlString = currentURL?.absoluteString {
+                            searchBarText = urlString
+                        }
+                    }
+                
+                Divider()
+                
+                HStack {
+                    
+                    TextField("Search Google or type a URL", text: $searchBarText)
+                        .keyboardType(.webSearch)
+                        .onSubmit {
+                            action = .load(searchBarText)
+                        }
+
+                    Button {
+                        searchBarText = ""
+                    } label: {
+                        Image(systemName: "xmark.circle.fill")
+                            .foregroundColor(.secondary)
+                    }
+                    .accessibilityIdentifier("clearButton")
+                    
+                }
+                .padding()
+                                
+                GeometryReader { geometry in
+                    Rectangle()
+                        .foregroundColor(estimatedProgress == 1.0 ? .clear : .secondary)
+                        .frame(width: geometry.size.width * CGFloat(estimatedProgress))
+                }
+                .frame(height: 4)
+                
+                Divider()
+                
+                HStack {
+
+                    Button(action: {
+                        action = .goBack
+                    }, label: {
+                        Image(systemName: "chevron.backward")
+                            .imageScale(.large)
+                            .frame(width: 44, height: 44, alignment: .center)
+
+                    })
+                    .disabled(!canGoBack)
+                    
+                    Button(action: {
+                        action = .goForward
+                    }, label: {
+                        Image(systemName: "chevron.forward")
+                            .imageScale(.large)
+                            .frame(width: 44, height: 44, alignment: .center)
+                    })
+                    .disabled(!canGoForward)
+
+                    Spacer()
+
+                    Button(action: {
+                        action = .reload
+                    }, label: {
+                        Image(systemName: "arrow.clockwise")
+                            .imageScale(.large)
+                            .frame(width: 44, height: 44, alignment: .center)
+                    })
+                    
+                }
+                .padding(8)
+            }
         }
-        .padding()
     }
 }
 
---
+++ PlainWebView-SwiftUI/WebView.swift
@@ -0,0 +1,79 @@
+//
+//  WebView.swift
+//  PlainWebView-SwiftUI
+//
+//  Created by Shigenari Oshio on 2023/10/24.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    typealias UIViewType = WKWebView
+    
+    let webView = WKWebView()
+    
+    enum Action {
+        case none, goBack, goForward, reload, load(String)
+    }
+        
+    @Binding var action: Action
+    @Binding var canGoBack: Bool
+    @Binding var canGoForward: Bool
+    @Binding var currentURL: URL?
+    
+    let setWebViewHandler: (WKWebView) -> Void
+    
+    func makeUIView(context: UIViewRepresentableContext<WebView>) -> WKWebView {
+        webView.navigationDelegate = context.coordinator
+        webView.allowsBackForwardNavigationGestures = true
+        
+        setWebViewHandler(webView)
+        
+        let homeUrl: URL = .init(string: "https://www.apple.com")!
+        webView.load(URLRequest(url: homeUrl))
+        
+        return webView
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: UIViewRepresentableContext<WebView>) {
+        switch action {
+        case .none:
+            break
+        case .goBack:
+            uiView.goBack()
+        case .goForward:
+            uiView.goForward()
+        case .reload:
+            uiView.reload()
+        case .load(let text):
+            if let url = URLComponents(string: text)?.url,
+               UIApplication.shared.canOpenURL(url) {
+                uiView.load(URLRequest(url: url))
+            } else {
+                if let url = URLComponents(string: "https://www.google.com/search?q=\(text)")?.url {
+                    uiView.load(URLRequest(url: url))
+                }
+            }
+        }
+        action = .none
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, WKNavigationDelegate {
+        var parent: WebView
+        
+        init(_ parent: WebView) {
+            self.parent = parent
+        }
+        
+        func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+            parent.canGoBack = webView.canGoBack
+            parent.canGoForward = webView.canGoForward
+            parent.currentURL = webView.url
+        }
+    }
+}
