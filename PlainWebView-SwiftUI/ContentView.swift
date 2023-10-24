60a381bf7155572db1094859126afef2f15a3885
S.Oshio <skw398@gmail.com>
2023-10-24 15:55:33 +0900

    Initial Commit

---
+++ PlainWebView-SwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PlainWebView-SwiftUI
+//
+//  Created by Shigenari Oshio on 2023/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PlainWebView-SwiftUI/PlainWebView_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlainWebView_SwiftUIApp.swift
+//  PlainWebView-SwiftUI
+//
+//  Created by Shigenari Oshio on 2023/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PlainWebView_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
